41a8ff5183fd20dadae82179b457e9fc0b47c45d
Nick Mello <43194417+xonmello@users.noreply.github.com>
2022-01-10 13:36:25 -0700

    Comments
    Added comments

--- main.go
+++ main.go
@@ -16,39 +16,44 @@ var totalTouches int = 0
 func getInput(gameState *RLBot.GameState, rlBot *RLBot.RLBot) *RLBot.ControllerState {
 	PlayerInput := &RLBot.ControllerState{}
 
+	// Get Location and Rotation information for easier use later
 	selfLocation := gameState.GameTick.Players[rlBot.PlayerIndex].Physics.Location
 	selfRotation := gameState.GameTick.Players[rlBot.PlayerIndex].Physics.Rotation
 	ballLocation := gameState.GameTick.Ball.Physics.Location
 
+	// If in air, point wheels down
 	if !gameState.GameTick.Players[rlBot.PlayerIndex].HasWheelContact {
 		PlayerInput.Roll = -1.0*selfRotation.Roll/math.Pi
 	}
 
+	// Find angle to ball
 	localX := ballLocation.X - selfLocation.X
 	localY := ballLocation.Y - selfLocation.Y
 	toBallAngle := math.Atan2(localY,localX)
 	steer := toBallAngle - selfRotation.Yaw
 
+	// Adjust to be within a -pi to pi range
 	if steer < -math.Pi {
 		steer += math.Pi * 2.0;
 	} else if steer >= math.Pi {
 		steer -= math.Pi * 2.0;
 	}
 
+	// If angle is greater than 1 radian, limit to full turn
 	if (steer > 1) {
 		steer = 1
 	} else if (steer < -1) {
 		steer = -1
 	}
-
 	PlayerInput.Steer = steer
 
+	// Drift if close to the ball and not very aligned
 	distanceToBall := math.Sqrt(localX * localX + localY * localY)
-
 	if distanceToBall < 300 && math.Abs(steer) > 0.3 {
 		PlayerInput.Handbrake = true
 	} 
 
+	// Go Forward
 	PlayerInput.Throttle = 1.0
 
 	return PlayerInput
